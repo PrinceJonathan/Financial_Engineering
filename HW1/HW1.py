5cb1bf9010d986a998bcec21aa5ac9862bf62d4e
princejonathan <a0926587680@gmail.com>
2020-03-09 12:14:02 +0800

    本金償還演算法

---
+++ HW1/HW1.py
@@ -0,0 +1,16 @@
+# 財務工程 108-2 作業 
+# 經濟四 李承翰
+p = int(input()) * 10000  #本金
+n_y = int(input())  #期數(年)
+i = int(input()) / 100  #年利率
+
+n_m = n_y * 12  #期數(月)
+p_m = p / n_m  #償還本金(月還
+p_i = 0  #本利累計
+print("期數(月)  本金(元)  利息(元)  本利累計(元)")
+for k in range(n_m):
+    i_m = p * (i / 12)  #利息(月)
+    p -= p_m
+    p_i_m = p_m + i_m  #本利累計(月)
+    p_i += p_i_m
+    print("第" + str(k + 1) + "期" + "   " + str(int(p_m)) + "     " + str(int(i_m)) + "      " + str(int(p_i)))
